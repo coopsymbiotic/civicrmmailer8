f38b8e8c9e580c4a36f356791b419e5fac132551
Mathieu Lutfy <mathieu@symbiotic.coop>
2018-12-12 11:36:26 -0500

    Port of civicrmmailer from D7

---
+++ README.md
@@ -0,0 +1,38 @@
+CiviCRM Mailer
+==============
+
+Once enabled, this module sends all Drupal emails through CiviCRM.
+
+This assumes:
+
+* All emails sent are sent to Drupal users (who therefore have a CiviCRM contact record).
+  * or .. emails are sent to CiviCRM contacts (ex: if using webform, your webform uses webform_civicrm to create a contact).
+* The site is using the DefaultMailer (when enabled, it replaces the default mailer).
+* The emails sent are plain HTML (they automatically go through nl2br).
+
+Why?
+
+* It logs all user emails to their contact record, which can be useful (welcome emails, password resets).
+* If using a third-party email service, you can configure only it once (in CiviCRM) and ignore Drupal settings.
+
+Needs testing:
+
+* Webform emails (to and cc).
+
+Support
+-------
+
+Please post bug reports in the issue tracker of this project on github: 
+https://github.com/coopsymbiotic/civicrmmailer/issues
+
+Commercial support via Coop SymbioTIC:  
+https://www.symbiotic.coop/en
+
+License
+-------
+
+(C) 2018 Mathieu Lutfy <mathieu@symbiotic.coop>  
+(C) 2018 Coop SymbioTIC <info@symbiotic.coop>
+
+Distributed under the terms of the GNU Affero General public license (AGPL).
+See LICENSE.txt for details.
---
+++ src/Plugin/Mail/Civicrmmailer.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mailsystem\Plugin\mailsystem\Dummy.
+ */
+
+namespace Drupal\maillog\Plugin\Mail;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Database\Database;
+use Drupal\Core\Mail\MailInterface;
+use Drupal\Core\Mail\Plugin\Mail\PhpMail;
+use Drupal\Core\Url;
+
+/**
+ * Sends and logs Drupal mails in CiviCRM.
+ *
+ * @Mail(
+ *   id = "civicrmmailer",
+ *   label = @Translation("CiviCRM Mailer Mail-Plugin"),
+ *   description = @Translation("CiviCRM Mailer Mail-Plugin for sending emails through CiviCRM.")
+ * )
+ */
+class Civicrmmailer implements MailInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function format(array $message) {
+    $default = new PhpMail();
+    return $default->format($message);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function mail(array $message) {
+    civicrm_initialize();
+
+    $contact = NULL;
+
+    // Drupal user emails are unique, so fetch the Drupal record
+    // then we'll fetch the civicrm contact record.
+    $uid = db_query('SELECT uid FROM {users} WHERE mail = :mail', [
+      ':mail' => $message['to'],
+    ])->fetchField();
+
+    if ($uid) {
+      $uf = civicrm_api3('UFMatch', 'getsingle', [
+        'uf_id' => $uid,
+      ]);
+
+      $contact = civicrm_api3('Contact', 'getsingle', [
+        'id' => $uf['contact_id'],
+      ]);
+    }
+
+    // The user might not exist. It could be a webform email.
+    // Find a matching email in CiviCRM, prioritize primary emails.
+    if (empty($contact)) {
+      $dao = CRM_Core_DAO::executeQuery('SELECT contact_id FROM civicrm_email WHERE email = %1 ORDER BY is_primary DESC', [
+        1 => [$message['to'], 'String'],
+      ]);
+
+      if ($dao->fetch()) {
+        $contact = civicrm_api3('Contact', 'getsingle', [
+          'id' => $dao->contact_id,
+        ]);
+      }
+    }
+
+    // FIXME: maybe return FALSE? for now, we prefer to get notifications
+    // when it fails.
+    if (empty($contact)) {
+      throw new Exception("Could not find Drupal user for {$message['to']}");
+    }
+
+    $formattedContactDetails = [];
+    $formattedContactDetails[] = $contact;
+
+    // By default Drupal sends text emails
+    // Should we try to detect if the message is already in HTML?
+    $html_message = nl2br($message['body']);
+
+    // Fetch the default organisation for the domain
+    $domain_id = CRM_Core_Config::domainID();
+
+    $default_org_id = CRM_Core_DAO::singleValueQuery('SELECT contact_id FROM civicrm_domain WHERE id = %1', [
+      1 => [$domain_id, 'Positive'],
+    ]);
+
+    list($sent, $activityId) = CRM_Activity_BAO_Activity::sendEmail(
+      $formattedContactDetails,
+      $message['subject'],
+      $message['body'], // text message
+      $html_message, // html body
+      NULL,
+      $default_org_id // used for the "from"
+    );
+
+    return TRUE;
+  }
+}
